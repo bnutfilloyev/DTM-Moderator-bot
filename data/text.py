f9d3fc558444ac857f58eaf01c0c2bc5f6062a8a
yoshlikmedia <yoshlikmedia@gmail.com>
2022-02-05 01:48:02 +0500

    add all text

--- data/text.py
+++ data/text.py
@@ -8,4 +8,44 @@ text = {
 
     'get_photo': "O'quvchining natijasini rasmini yuboring.",
 
+    'get_name': "🧑‍💻FIO ni kiriting.\n\n"
+                "<b>⚠️Diqqat:</b> Ma'lumotlarni kiritishda xatoga yo'l qo'ymang! "
+                "Aks holda, sizning natijangiz chiqishiga kafolat bermaymiz.",
+
+    'get_subject': "🔰Blokingizni kiriting.",
+
+    'get_id': "🔑ID raqamingizni kiriting.",
+
+    'set_id': "⚠️Sizning ID raqamingiz va o'tirish o'rningiz: \n\n"
+              "📝 <b>{}\n</b>"
+              "🔠 <b>{}\n</b>"
+              "🆔 <code>{}</code>\n"
+              "📍 <b>{}</b>\n\n"
+              "✅<i>Natijalarni ko'rish uchun shu IDdan foydalansiz "
+              "va quyida keltirilgan o'ringa o'tirishingizni so'rab qolamiz.</i>",
+
+    'result_id': "🔑O'quvchi ID raqamini kiriting:\n\n"
+                 "<b>⚠️Diqqat:</b> Ma'lumotlarni kiritishda xatoga yo'l qo'ymang! "
+                 "Aks holda, o'quvchi natijasi chiqishiga kafolat bermaymiz.",
+
+    'result_data': "Ma'lumotlarni quyidagi ko'rinishda yuboring.\n\n"
+                   "Ona tili(Majburiy): to'g'ri javoblar soni\n"
+                   "O'zbekiston tarixi(Majburiy): to'g'ri javoblar soni\n"
+                   "Matematika(Majburiy): to'g'ri javoblar soni\n"
+                   "Blok1: to'g'ri javoblar soni\n"
+                   "Blok2: to'g'ri javoblar soni\n"
+                   "Umumiy ball: Umumiy ball\n\n"
+                   "⚠️<b>Diqqat:</b> Ma'lumotlar qabul qilingan ko'rinishda saqlanadi. "
+                   "Xatosiz va to'g'ri yozing!",
+
+    'result': "<b>🔑ID: </b>{}\n"
+              "<b>🔠Block: </b>{}\n"
+              "📍<b>Joy: </b>{}\n\n"
+              "{}",
+
+    'not_found': "Natijangizni bilish uchun ID raqamini kiriting:\n\n"
+                 "Bu funktsiya faqat natijalar chiqqandan so'ng amal qiladi. "
+                 "Agar siz botdan ro'yxatdan o'tmagan bo'lsangiz /imtihon ustiga bosing",
+
+    'error': "Sizning natijangiz hali chiqmagan."
 }
